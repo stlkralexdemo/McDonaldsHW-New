d5582529ae8ef58cfb732e13f476cb4857e1d2df
student <student@localhost>
2019-01-13 22:31:00 +0300

    JdbcTemplate

--- src/main/java/ru/itpark/webstore/repository/ProductRepositoryJDBCImpl.java
+++ src/main/java/ru/itpark/webstore/repository/ProductRepositoryJDBCImpl.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@Repository
+//@Repository
 public class ProductRepositoryJDBCImpl implements ProductRepository {
     private final DataSource dataSource;
 

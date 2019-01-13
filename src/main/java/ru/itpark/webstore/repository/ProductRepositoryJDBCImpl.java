75bc94de67e3753a94563331d7e8527136005bc1
student <student@localhost>
2019-01-13 15:32:14 +0300

    Try with resources

--- src/main/java/ru/itpark/webstore/repository/ProductRepositoryJDBCImpl.java
+++ src/main/java/ru/itpark/webstore/repository/ProductRepositoryJDBCImpl.java
@@ -5,10 +5,7 @@ import ru.itpark.webstore.domain.Product;
 import ru.itpark.webstore.exception.DbException;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -23,70 +20,83 @@ public class ProductRepositoryJDBCImpl implements ProductRepository {
 
     @Override
     public List<Product> getAll() {
-        Connection connection = null;
-        Statement statement = null;
-        ResultSet resultSet = null;
+        List<Product> products = new ArrayList<>();
+        // Шаблонный код -> Template
+        try (Connection connection = dataSource.getConnection()) {
+            try (Statement statement = connection.createStatement();) {
+                try (ResultSet resultSet = statement.executeQuery("SELECT id, name, price FROM products");) {
+                    while (resultSet.next()) {
+                        int id = resultSet.getInt("id");
+                        String name = resultSet.getString("name");
+                        int price = resultSet.getInt("price");
+                        products.add(new Product(id, name, price));
+                    }
 
-        try {
-            List<Product> products = new ArrayList<>();
-
-            connection = dataSource.getConnection();
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery("SELECT id, name, price FROM products");
-            while (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                String name = resultSet.getString("name");
-                int price = resultSet.getInt("price");
-                products.add(new Product(id, name, price));
+                    return products;
+                }
             }
-            resultSet.close();
-            statement.close();
-            connection.close();
-
-            return products;
         } catch (SQLException e) {
             // rethrow exception:
             // выкидываем non-checked exception + заворачивает старое
             throw new DbException(e);
-        } finally {
-            if (resultSet != null) {
-                try {
-                    resultSet.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            if (statement != null) {
-                try {
-                    statement.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
         }
     }
 
     @Override
     public Optional<Product> getById(int id) {
-        return Optional.empty();
+        // Шаблонный код -> Template
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement("SELECT id, name, price FROM products WHERE id = ?");) {
+                statement.setInt(1, id);
+                try (ResultSet resultSet = statement.executeQuery();) {
+                    if (resultSet.next()) {
+                        String name = resultSet.getString("name");
+                        int price = resultSet.getInt("price");
+                        return Optional.of(new Product(id, name, price));
+                    }
+
+                    return Optional.empty();
+                }
+            }
+        } catch (SQLException e) {
+            throw new DbException(e);
+        }
     }
 
     @Override
     public void save(Product item) {
+        // Шаблонный код -> Template
+        try (Connection connection = dataSource.getConnection()) {
+            if (item.getId() == 0) {
+                try (PreparedStatement statement = connection.prepareStatement("INSERT INTO products (name, price) VALUES (?, ?)");) {
+                    statement.setString(1, item.getName());
+                    statement.setInt(2, item.getPrice());
+                    statement.executeUpdate();
+                }
+                return;
+            }
 
+            try (PreparedStatement statement = connection.prepareStatement("UPDATE products SET name = ?, price = ? WHERE id = ?");) {
+                statement.setString(1, item.getName());
+                statement.setInt(2, item.getPrice());
+                statement.setInt(3, item.getId());
+                statement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new DbException(e);
+        }
     }
 
     @Override
     public void removeById(int id) {
-
+        // Шаблонный код -> Template
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement("DELETE FROM products WHERE id = ?");) {
+                statement.setInt(1, id);
+                statement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new DbException(e);
+        }
     }
 }
